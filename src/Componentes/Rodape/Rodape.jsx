e00925fdf8afcc8b125513a987f1649146dc0ff0
Victor Malmann da Cunha <victorcunhasc@gmail.com>
2024-11-13 22:28:56 -0300

    Continuação do rodapé

--- src/Componentes/Rodape/Rodape.jsx
+++ src/Componentes/Rodape/Rodape.jsx
@@ -1,9 +1,36 @@
+import { MdLocalPhone } from "react-icons/md";
 import "./Rodape.css";
+import { MdWhatsapp } from "react-icons/md";
+import { MdEmail } from "react-icons/md";
+import { MdFacebook } from "react-icons/md";
+import { FaYoutube } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 const Rodape = () => {
   return (
-    <footer className="rodape-footer">
-      <h3>Contato</h3>
+    <footer className="rodape">
+      <div className="rodape-contato">
+        <h3>Contato</h3>
+        <p>
+          <MdLocalPhone />
+          (48) 9999-9999
+        </p>
+        <p>
+          <MdWhatsapp />
+          (48) 9999-9999
+        </p>
+        <p>
+          <MdEmail />
+          ciadomelsenai@gmail.com
+        </p>
+      </div>
+      <div>
+        <div>
+          <MdFacebook />
+          <FaYoutube />
+          <FaInstagram />
+        </div>
+      </div>
     </footer>
   );
 };
