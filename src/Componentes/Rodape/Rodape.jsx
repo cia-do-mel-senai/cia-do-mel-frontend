bc3c64bb05d73327365ce7ad727e11051a95d965
Victor <victorcunhasc@gmail.com>
2024-11-16 10:24:28 -0300

    Finalização do rodapé

--- src/Componentes/Rodape/Rodape.jsx
+++ src/Componentes/Rodape/Rodape.jsx
@@ -10,26 +10,30 @@ const Rodape = () => {
   return (
     <footer className="rodape">
       <div className="rodape-contato">
-        <h3>Contato</h3>
-        <p>
-          <MdLocalPhone />
-          (48) 9999-9999
-        </p>
-        <p>
-          <MdWhatsapp />
-          (48) 9999-9999
-        </p>
-        <p>
-          <MdEmail />
-          ciadomelsenai@gmail.com
-        </p>
-      </div>
-      <div>
+        <h3 style={{ fontSize: "0.8rem" }}>Contato</h3>
         <div>
-          <MdFacebook />
-          <FaYoutube />
-          <FaInstagram />
+          <MdLocalPhone size={10} color="grey" />
+          <p>(48) 9999-9999</p>
         </div>
+        <div>
+          <MdWhatsapp size={10} color="rgb(30, 255, 30)" />
+          <p>(48) 9999-9999</p>
+        </div>
+        <div>
+          <MdEmail size={10} color="#50b3ff" />
+          <p>ciadomelsenai@gmail.com</p>
+        </div>
+      </div>
+      <div className="rodape-fundo">
+        <div>
+          <MdFacebook size={10} color="#678dff" />
+          <FaYoutube size={10} color="red" />
+          <FaInstagram size={10} color="#ff67d6" />
+        </div>
+        <p>
+          Copyright©{new Date().getFullYear()}-Todos os direitos reservados-Cia
+          do Mel
+        </p>
       </div>
     </footer>
   );
