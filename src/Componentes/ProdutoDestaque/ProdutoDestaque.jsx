9c7fc85753251fc1d063c773d5dba15917250a87
Victor <victorcunhasc@gmail.com>
2024-11-16 12:09:46 -0300

    Finalização do rodapé e da tela principal

---
+++ src/Componentes/ProdutoDestaque/ProdutoDestaque.jsx
@@ -0,0 +1,18 @@
+import "./ProdutoDestaque.Css";
+import { IoMdSearch } from "react-icons/io";
+
+const ProdutoDestaque = ({ imagem, nome }) => {
+  return (
+    <div className="produto-destaque-container">
+      <img src={imagem} />
+      <p>
+        <b>{nome}</b>
+      </p>
+      <button>
+        Ver Mais <IoMdSearch size={20} />
+      </button>
+    </div>
+  );
+};
+
+export default ProdutoDestaque;
--- src/Paginas/Principal/Principal.jsx
+++ src/Paginas/Principal/Principal.jsx
@@ -1,6 +1,78 @@
+import ProdutoDestaque from "../../Componentes/ProdutoDestaque/ProdutoDestaque";
 import "./Principal.css";
 
 const Principal = () => {
-  return <div className="principal-container"></div>;
+  return (
+    <div className="principal-container">
+      <div className="produtos-destaque-principal">
+        <div className="produtos-destaque">
+          <ProdutoDestaque
+            imagem={
+              "https://www.lojaapicola.com.br/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/m/e/mel_pote_1kg_frente.jpg"
+            }
+            nome={"Pote de Mel"}
+          />
+          <ProdutoDestaque
+            imagem={
+              "https://www.lojaapicola.com.br/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/m/e/mel_pote_1kg_frente.jpg"
+            }
+            nome={"Pote de Mel"}
+          />
+          <ProdutoDestaque
+            imagem={
+              "https://www.lojaapicola.com.br/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/m/e/mel_pote_1kg_frente.jpg"
+            }
+            nome={"Pote de Mel"}
+          />
+          <ProdutoDestaque
+            imagem={
+              "https://www.lojaapicola.com.br/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/m/e/mel_pote_1kg_frente.jpg"
+            }
+            nome={"Pote de Mel"}
+          />
+        </div>
+        <button
+          style={{
+            backgroundColor: "var(--cor-amarelo)",
+            padding: "10px",
+            width: "70%",
+            border: "none",
+            borderRadius: "20px",
+            fontSize: "1.3rem",
+            fontWeight: "bold",
+            marginTop: "15px",
+          }}
+        >
+          Ver Todos
+        </button>
+      </div>
+      <div className="texto-pagina-principal">
+        <p>
+          <h2>Cia do Mel</h2>
+          Bem-vindo à Cia do Mel!
+          <br />
+          <br />
+          Aqui, você encontra mel 100% puro, extraído de forma sustentável para
+          oferecer sabor e qualidade excepcionais.
+          <br />
+          <br />
+          Por que escolher a Cia do Mel?
+          <br />
+          <br />
+          - Mel fresco e natural, direto das melhores colmeias. <br />-
+          Variedade de tipos e sabores. <br />- Compromisso com a
+          sustentabilidade e preservação das abelhas.
+          <br />
+          <br />
+          Explore nossa seleção e descubra os benefícios do mel para sua saúde e
+          bem-estar.
+          <br />
+          <br />
+          Compre agora e aproveite nosso frete grátis para todo o Brasil em
+          compras acima de R$99!
+        </p>
+      </div>
+    </div>
+  );
 };
 export default Principal;
